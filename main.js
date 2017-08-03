fb9811a90c064597806911cd170bbc8478364f8c
rikumax25 <jorgev259@outlook.es>
2017-08-02 23:30:42 -0600

    wakeup

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ app.listen(port, function() {});
 
 client.on('ready', () => {
   console.log('I am ready!');
-    setInterval(util.wakeup,1200000,client);
+    setInterval(util.wakeup,600000,client);
     util.wakeup(client);
 });
 
