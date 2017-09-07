33bdb888c2dea87ecf54640403d29f492599ea72
rikumax25 <jorgev259@outlook.es>
2017-09-06 21:59:25 -0600

    >poll command finished

--- main.js
+++ main.js
@@ -304,13 +304,17 @@ client.on('message', message => {
 
                         case "poll":
                             message.delete();
-                            message.channel.send("Type the options for you poll. Example: `option 1|Option 2|Non numbered option`");
+                            var optionMessage;
+                            message.reply("type the options for you poll. Example: `option 1|Option 2|Non numbered option`").then(mDelete => optionMessage = mDelete);
                             const collector = message.channel.createMessageCollector(
                               m => m.author.id == message.author.id,
                               { max: 1 }
                             );
                             collector.on('collect', m => {
                                 m.delete();
+                                optionMessage.delete();
+                                var time = parseInt(param[1]);
+                                param.shift();
                                 param.shift();
                                 var text = param.join(" ") + ": ";
                                 var count = 0;
@@ -321,17 +325,44 @@ client.on('message', message => {
                                 })
                                 message.channel.send(text).then(poll => {
                                     util.react(0,count,poll);
+                                    message.channel.send("Poll will be over in "  + time + " minutes").then(pollTime => {
+                                        setTimeout(function(){
+                                            pollTime.delete();
+                                            var results = []
+                                            poll.reactions.array().forEach(function(reaction){
+                                                results.push({"name":options[(parseInt(reaction._emoji.name.charAt(0)))-1],"points":parseInt(reaction.count)});
+                                            })
+
+                                            results.sort(function(a,b){
+                                                return a.points - b.points
+                                            })
+
+                                            var winners = [results[results.length-1]];
+                                            for(var i=results.length-2;i>=0;i--){
+                                                if(results[i].points == results[i+1].points){
+                                                    winners.push(results[i]);
+                                                }else{
+                                                    i=-1;
+                                                }
+                                            }
+
+                                            if(winners.length == 1){
+                                                message.channel.send("Poll has finished. *drumrolls*\n" + param.join(" ") + " " + winners[0].name " is the winner!");
+                                            }else{
+                                                var response = "Poll has finished. " + param.join(" ") + "\n*drumrolls*\n There was a tie between " + winners[0].name;
+                                                for(var i=1;i<winners.length;i++){
+                                                    if(i+1 == winners.length){
+                                                        response += " and " + winners[i].name;
+                                                    }else{
+                                                        response += ", " + winners[i].name;
+                                                    }
+                                                }
+                                                message.channel.send(response);
+                                            }
+                                        },time*60000)
+                                    });
                                 });
                             });
-                            /*
-                            var count = 0;
-                            options.forEach(function(option){
-                                text += reactionNumbers[count] + option + " ";
-                            })
-                            message.channel.send(text);
-                            message.delete();
-                            */
-
                             break;
 
                         case 'play':
