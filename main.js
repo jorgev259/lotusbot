091db0480c64def18c401b9ab4c96e8aa10b41fd
rikumax25 <jorgev259@outlook.es>
2017-09-16 12:47:38 -0600

    eval command

--- main.js
+++ main.js
@@ -427,6 +427,21 @@ client.on('message', message => {
                             message.reply(response);
                             break;
 
+                        case "eval":
+                            try {
+                              const code = param.shift().join(" ");
+                              let evaled = eval(code);
+
+                              if (typeof evaled !== "string")
+                                evaled = require("util").inspect(evaled);
+
+                              message.channel.send(clean(evaled), {code:"xl"});
+                            } catch (err) {
+                              message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+                            }
+
+                            break;
+
                         case "default":
                         default:
                             if(config["404"] === "true"){
