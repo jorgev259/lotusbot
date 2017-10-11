afad3ef455f8c4d8d4a1153886d8b3cf3a99b10b
root <jorgev259@outlook.es>
2017-10-11 21:56:17 +0000

    fix quote commands

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var reactions = ["rage","thinking","blush","stuck_out_tongue_closed_eyes","heart
 
 var commands = require("../data/commands.json");
 var perms = require("../data/perms.json");
-//var quotes = require("../data/quotes.json");
+var quotes = require("../data/quotes.json");
 //var art= require("../data/art.json");
 var config = require("../data/config.json");
 
@@ -246,11 +246,11 @@ client.on('message', message => {
                 message.channel.messages.fetch(snowflake).then(function(quote){
                     var embed = new Discord.MessageEmbed()
                     .setColor(color)
-                    .setDescription(quote.content + "\nQuote id: " + Object.keys(quotes).length)
+                    .setDescription(quote.content + "\nQuote id: " + quotes.length)
                     .setTitle("#" + quote.channel.name)
                     .setThumbnail(thumb)
-                    .setAuthor(quote.author.username, quote.author.displayAvatarURL());
-                    quotes[Object.keys(quotes).length] = {
+                    .setAuthor(quote.author.username, quote.author.avatarURL());
+                    quotes[quotes.length] = {
                         "desc":quote.content,
                         "title":"#" + quote.channel.name,
                         "author":quote.author.username,
@@ -259,15 +259,16 @@ client.on('message', message => {
                         "avatar": quote.author.defaultAvatarURL
                     };
                     //save line
-                    message.guild.channels.find("name","memorabilia").send({embed});
-                    message.delete();
+                    util.save(quotes,"quotes");
                     message.reply(" has recorded your message in the books of history <@" + quote.author.id + ">");
+                    message.delete();
+                    message.guild.channels.find("name","memorabilia").send({embed});
                 });
                 break;
 
             case "quote":
                 var quote = quotes[parseInt(param[1])];
-                if(quote != undefined){
+                if(quote != undefined && quote != null){
                     var embed = new Discord.MessageEmbed()
                     .setColor(quote.color)
                     .setDescription(quote.desc)
