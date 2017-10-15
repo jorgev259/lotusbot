d5167bda72c6d8745606ea3c5e61a0a8bd54ea4a
fandombear <31215492+fandombear@users.noreply.github.com>
2017-10-15 20:36:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
 
 client.on("guildMemberAdd", (member) => {
     member.addRoles([member.guild.roles.find("name", "Customers"),member.guild.roles.find("name","[1]")]);
-    member.guild.channels.find("name","chat").send("Welcome to Fandom Circle, <@" + member.id + ">! Have Fun");
+    member.guild.channels.find("name","main-lounge").send("Welcome to Fandom Circle, <@" + member.id + ">! Have Fun");
 });
 
 client.on("messageReactionAdd",(reaction,user)=>{
