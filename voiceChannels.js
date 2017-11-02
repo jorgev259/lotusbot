0621075278d95e73d50515d5f6b3a78775451f92
rikumax25 <jorgev259@outlook.es>
2017-11-02 09:22:15 -0600

    voicechannel crash fix

--- voiceChannels.js
+++ voiceChannels.js
@@ -6,21 +6,23 @@ module.exports = function(client){
             var newTextChannel = channels[newMember.voiceChannel.name];
             var oldTextChannel = channels[oldMember.voiceChannel.name];
 
-            if(oldMember.voiceChannelID == undefined && newMember.voiceChannelID != undefined){
-                var newTextChannel = channels[newMember.voiceChannel.name];
-                if(newTextChannel != undefined){
-                    newMember.addRole(newMember.guild.roles.find("name",newTextChannel));
-                }
-            }else if(oldMember.voiceChannelID != undefined && newMember.voiceChannelID != undefined && oldMember.voiceChannelID != newMember.voiceChannelID){
-                if(newtextChannel != undefined){
-                    newMember.addRole(newMember.guild.roles.find("name",newTextChannel));
-                }
-                if(oldtextChannel != undefined){
-                    newMember.removeRole(newMember.guild.roles.find("name",oldTextChannel));
-                }
-            }else if(oldMember.voiceChannelID != undefined && newMember.voiceChannelID == undefined){
-                if(oldtextChannel != undefined){
-                    newMember.removeRole(newMember.guild.roles.find("name",oldTextChannel));
+            if(newTextChannel != undefined || oldTextChannel!=undefined){
+                if(oldMember.voiceChannelID == undefined && newMember.voiceChannelID != undefined){
+                    var newTextChannel = channels[newMember.voiceChannel.name];
+                    if(newTextChannel != undefined){
+                        newMember.addRole(newMember.guild.roles.find("name",newTextChannel));
+                    }
+                }else if(oldMember.voiceChannelID != undefined && newMember.voiceChannelID != undefined && oldMember.voiceChannelID != newMember.voiceChannelID){
+                    if(newtextChannel != undefined){
+                        newMember.addRole(newMember.guild.roles.find("name",newTextChannel));
+                    }
+                    if(oldtextChannel != undefined){
+                        newMember.removeRole(newMember.guild.roles.find("name",oldTextChannel));
+                    }
+                }else if(oldMember.voiceChannelID != undefined && newMember.voiceChannelID == undefined){
+                    if(oldtextChannel != undefined){
+                        newMember.removeRole(newMember.guild.roles.find("name",oldTextChannel));
+                    }
                 }
             }
         })
