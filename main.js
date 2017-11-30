07ed2d5605805bac0108498622c6050c7b7cf152
rikumax25 <jorgev259@outlook.es>
2017-11-30 09:56:36 -0600

    fuck sqlite

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const Discord = require('discord.js');
 var fs = require("fs");
 const Canvas = require('canvas')
 const download = require('image-downloader')
-const economy = require('discord-eco-chito');
-economy.start("../shiro/userData.sqlite");
 
 Canvas.registerFont("./font/BebasNeue Bold.ttf",{family:"BebasNeue Bold"})
 Canvas.registerFont("./font/Mizo Arial.ttf",{family:"Mizo Arial"})
@@ -347,44 +345,42 @@ client.on('message', message => {
 						dest: `temp/${pfMember.id}.png`
 					}
 
-					economy.fetchBalance(pfMember.id + message.guild.id).then((i) => {
-						download.image(options).then(({ filename, image }) => {
-							var id = i.userID.substring(0,18)
-							var profile = Canvas.createCanvas(1059,787);
-							var pfCtx = profile.getContext('2d');
-							var img = new Canvas.Image();
+					download.image(options).then(({ filename, image }) => {
+						var id = pfMember.id
+						var profile = Canvas.createCanvas(1059,787);
+						var pfCtx = profile.getContext('2d');
+						var img = new Canvas.Image();
 
-							img.src = fs.readFileSync(bg);
-							pfCtx.drawImage(img,0,0);
+						img.src = fs.readFileSync(bg);
+						pfCtx.drawImage(img,0,0);
 
-							img.src= fs.readFileSync("./images/profile.png");
-							pfCtx.drawImage(img,0,0);
+						img.src= fs.readFileSync("./images/profile.png");
+						pfCtx.drawImage(img,0,0);
 
-							img.src= fs.readFileSync(`temp/${id}.png`);
-							pfCtx.drawImage(img,72,296,195,195);
-							fs.unlink(`temp/${id}.png`)
+						img.src= fs.readFileSync(`temp/${id}.png`);
+						pfCtx.drawImage(img,72,296,195,195);
+						fs.unlink(`temp/${id}.png`)
 
-							img.src= fs.readFileSync("./images/bar1.png");
-							var percent;
-							if(exp[id].lvl > 0) {
-								percent = ((exp[id].exp - levels[exp[id].lvl -1].exp) / (levels[exp[id].lvl].exp - levels[exp[id].lvl -1].exp))
-							}else{
-								percent = ((exp[id].exp) / (levels[0].exp))
-							}
-							pfCtx.drawImage(img,312,461,(435*percent),26);
+						img.src= fs.readFileSync("./images/bar1.png");
+						var percent;
+						if(exp[id].lvl > 0) {
+							percent = ((exp[id].exp - levels[exp[id].lvl -1].exp) / (levels[exp[id].lvl].exp - levels[exp[id].lvl -1].exp))
+						}else{
+							percent = ((exp[id].exp) / (levels[0].exp))
+						}
+						pfCtx.drawImage(img,312,461,(435*percent),26);
 
-							pfCtx.font = '180px "BebasNeue Bold"';
-							pfCtx.fillStyle = '#000000';
-							pfCtx.fillText(exp[id].lvl, 90,645);
+						pfCtx.font = '180px "BebasNeue Bold"';
+						pfCtx.fillStyle = '#000000';
+						pfCtx.fillText(exp[id].lvl, 90,645);
 
-							pfCtx.font = '30px "Mizo Arial"';
-							pfCtx.fillStyle = '#ffffff';
-							pfCtx.fillText(nick.join(" "), 353,440);
-							pfCtx.fillText(exp[id].exp.toString() + " / " + levels[exp[id].lvl].exp, 506,530);
-							pfCtx.fillText(i.money, 506,568);
+						pfCtx.font = '30px "Mizo Arial"';
+						pfCtx.fillStyle = '#ffffff';
+						pfCtx.fillText(nick.join(" "), 353,440);
+						pfCtx.fillText(exp[id].exp.toString() + " / " + levels[exp[id].lvl].exp, 506,530);
+						pfCtx.fillText(exp[id].money, 506,568);
 
-							message.channel.send(new Discord.MessageAttachment(profile.toBuffer(),"profile.png"))
-						})
+						message.channel.send(new Discord.MessageAttachment(profile.toBuffer(),"profile.png"))
 					})
 					break;
 
