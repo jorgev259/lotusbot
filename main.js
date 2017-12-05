82dfd6302472443fbe352a060567c8dd15c836f4
rikumax25 <jorgev259@outlook.es>
2017-12-05 16:04:46 -0600

    disable this

--- main.js
+++ main.js
@@ -168,54 +168,6 @@ client.on('message', message => {
 						}
 						break;
 
-					case "perms":
-						var name = param[1];
-						var type = param[2];
-						param.shift();
-						param.shift();
-						param.shift();
-						if(perms[name] != undefined){
-							switch(type){
-								case "add":
-									if(message.mentions.users.size > 0){
-										perms[name].user.push(message.mentions.users.first().id);
-									}else if(message.mentions.channels.size > 0){
-										perms[name].channel.push(message.mentions.channels.first().id);
-									}else{
-										perms[name].role.push(param.join(" "));
-									}
-									util.save(perms,"perms");
-									message.reply(param.join(" ") + " is now allowed to use " + name);
-									break;
-
-									/*case "remove":
-                                            result[0].perms = result[0].perms.filter(e => e !== param.join(" ") );
-                                            perms.save(result[0]);
-                                            message.reply("Removed " + param.join(" ") + " from the command " + name);
-                                            break;*/
-							}
-						}else{
-							switch(type){
-								case "add":
-									perms[name] = {"user":[], "role":[], "channel":[]};
-
-									if(message.mentions.users.size > 0){
-										perms[name].user.push(message.mentions.users.first().id);
-									}else if(message.mentions.channels.size > 0){
-										perms[name].channel.push(message.mentions.channels.first().id);
-									}else{
-										perms[name].role.push(param.join(" "));
-									}
-
-									util.save(perms,"perms");
-									message.reply(param.join(" ") + " is now allowed to use " + name);
-									break;
-
-								case "delete":
-									message.reply("This command has no permissions set");
-							}
-						}
-						break;
 
 					case "addquote":
 						var color;
@@ -434,8 +386,8 @@ client.on('message', message => {
 
 					case "eval":
 						try {
-							var exp = json.readFileSync("../data/exp.json");
-							var inventory = json.readFileSync("../data/inventory.json");
+							/*var exp = json.readFileSync("../data/exp.json");
+							var inventory = json.readFileSync("../data/inventory.json");*/
 							param.shift();
 							const code = param.join(" ");
 							let evaled = eval(code);
