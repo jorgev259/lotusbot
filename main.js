fd12e0af5ffa829faccdf2fcb209bbdd2d6aa6bc
rikumax25 <jorgev259@outlook.es>
2017-12-08 10:23:41 -0600

    fix join role

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ client.on("guildMemberAdd", (member) => {
 
 	var exp = json.readFileSync("../data/exp.json");
 	member.guild.channels.find("name","main-lounge").send(`Welcome to Fandom Circle, <@${member.id}>! Have Fun`);
-	member.addRoles([member.guild.roles.find("name", "☕ Customers"),member.guild.roles.find("name",`[${exp[member.id].lvl}]`)],"User join");
+	member.addRoles(member.guild.roles.find("name", "☕ - Customers"),"User join");
 });
 
 /*client.on("messageReactionAdd",(reaction,user)=>{
