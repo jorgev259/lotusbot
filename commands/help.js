73059c9147758997803437b0df4a6e1b5f644446
rikumax25 <jorgev259@outlook.es>
2017-12-11 16:36:27 -0600

    blank parameter

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 module.exports = {
     desc:"This command displays information about a command. Usage: >help command",
     execute(client, message, param){
-        if (client.commands.has(param[1].toLowerCase())){
+        if (param[1] && client.commands.has(param[1].toLowerCase())){
             message.channel.send(client.commands.get(param[1].toLowerCase()).desc);
-        }else{
+        }else if(param[1]){
             message.channel.send(`${param[1]} couldnt be found`)
         }
     }
