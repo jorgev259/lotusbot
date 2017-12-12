5588088516af26b28098d073479565331923d8e0
rikumax25 <jorgev259@outlook.es>
2017-12-12 10:40:02 -0600

    update and realod commands

--- commands/update.js
+++ commands/update.js
@@ -4,9 +4,12 @@ const git = require('simple-git')(path.resolve(__dirname,"../"));
 module.exports = {
     desc:"This is a description",
     execute(client, message, param){
-        git.pull().then((res)=>{
-            console.log("err2123123");
-            console.log(res);
-        })
+        message.delete();
+
+        message.channel.send("Downloading changes.....").then(m=>{
+            git.pull().then(()=>{
+                m.edit("Git pull successful!");
+            })
+        })      
     }
 }
