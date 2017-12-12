680d2e91db642048f352c73304284e683765d676
rikumax25 <jorgev259@outlook.es>
2017-12-12 15:52:40 -0600

    disable command

---
+++ commands/disable.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+module.exports = {
+    desc:"This is a description",
+    execute(client, message, param){
+        var commandName = param[1].toLowerCase();
+        if(commandName){
+            message.delete();
+
+            var modulePath = `${__dirname}/${commandName}.js`;
+            if(client.commands.has(commandName) && fs.existsSync(modulePath)){
+                client.commands.delete(commandName);
+                message.channel.send(`Disabled ${commandName}!`);
+            }else{
+                message.channel.send(`${commandName}.js couldnt be found.`)
+            }
+        }
+    }
+}
