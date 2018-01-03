6835912d68fc53b2b70265a74c3135f096376fbf
rikumax25 <jorgev259@outlook.es>
2018-01-03 09:00:41 -0600

    BADGES!

--- commands/profile.js
+++ commands/profile.js
@@ -67,6 +67,21 @@ module.exports = {
             pfCtx.fillText(exp[id].exp.toString() + " / " + levels[exp[id].lvl].exp, 506,530);
             pfCtx.fillText(exp[id].money, 506,568);
 
+            if(exp[id].badges && exp[id].badges.length > 0){
+                for(var i=0;i<exp[id].badges.length;i++){
+                    if(exp[id].badges[i] != undefined){
+                        var row = 0;
+                        if(i>2) row += Math.floor(i>3);
+                        var column = i - (row*3)
+                        var y = 430 + (75*row);
+                        var x = 775 + (80*column);
+
+                        img.src=fs.readFileSync(`images/badges/${exp[id].badges[i]}.png`);
+                        pfCtx.drawImage(img,x,y,70,70);
+                    }
+                }
+            }
+
             message.channel.send(new Discord.MessageAttachment(profile.toBuffer(),"profile.png"))
         })
     }
---
+++ commands/unequip.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var util = require("../utilities.js");
+var json = require('jsonfile');
+
+module.exports = {
+    desc:"This is a description",
+    alias:["bg"],
+    execute(client, message, param){
+        var inventory = json.readFileSync("../data/inventory.json");
+        var exp = json.readFileSync("../data/exp.json");
+        if(exp[message.author.id].badges == undefined) {exp[message.author.id].badges = [];util.save(exp,"exp")}
+
+        if(param.length > 1){
+            if(!Number.isInteger(param[1])) return message.channel.send(`Invalid number`) 
+
+            var slot = parseInt(param[1]);
+            if(exp[message.author.id].badges[slot - 1]){
+                exp[message.author.id].badges[slot - 1] = undefined;
+                message.channel.send(`The slot number ${slot} has been emptied!`)
+                util.save(exp,"exp");
+            }else{
+                message.channel.send(`The slot number ${slot} has nothing equipped on it!`)
+            }
+        }else{
+            message.channel.send("You forgot the number of slot you want to empty. Usage: >unequip <slot>");
+        }
+    }
+}
