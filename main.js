06b6dfc459f38813fb9bbc3afe5d3418034174ec
rikumax25 <jorgev259@outlook.es>
2018-01-04 09:48:21 -0600

    idk

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands');
 const commonCommands = fs.readdirSync('./commonCommands');
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+	const command = require(`./commands/${file}`);
 	client.commands.set(file.split(".js")[0], command);
 	if(command.alias){
 		command.alias.forEach(alias => client.commands.set(alias, command))
