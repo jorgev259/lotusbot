821e57b94f2665b51ba7bf0c5cd15724e740d897
rikumax25 <jorgev259@outlook.es>
2018-01-10 16:41:27 -0600

    debugging

--- commands/profile.js
+++ commands/profile.js
@@ -72,9 +72,12 @@ module.exports = {
 
                 if(exp[id].badges && exp[id].badges.length > 0){
                     for(var i=0;i<exp[id].badges.length;i++){
-                        if(exp[id].badges[i] != undefined){
+                        if(exp[id].badges[i]){
                             var row = 0;
                             if(i>2) row += Math.floor(i>3);
+
+                            console.log(column);
+                            console.log(row);
                             var column = i - (row*3)
                             var y = 430 + (75*row);
                             var x = 775 + (80*column);
