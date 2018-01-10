770b2676732259bde35cdeb595424a8a9db981a9
rikumax25 <jorgev259@outlook.es>
2018-01-10 11:35:47 -0600

    little changes

--- commonCommands/help.js
+++ commonCommands/help.js
@@ -3,19 +3,22 @@ var util = require("../utilities.js")
 module.exports = {
     desc:"This command displays information about a command. Usage: >help command",
     execute(client, message, param){
-
-        if(param[1]){
-            if (client.commands.has(param[1].toLowerCase())){
-                message.author.send(client.commands.get(param[1].toLowerCase()).desc);
+        try{
+            if(param[1]){
+                if (client.commands.has(param[1].toLowerCase())){
+                    message.author.send(client.commands.get(param[1].toLowerCase()).desc);
+                }
+            }else{
+                var commands = ""
+                Array.from(client.commands.keys()).forEach(idName => {
+                    if(util.permCheck(message,idName)){
+                        commands += `${idName}: ${client.commands.get(idName).desc}\n`
+                    }               
+                })
+                message.author.send(commands, {code:"xl",split:true});
             }
-        }else{
-            var commands = ""
-            Array.from(client.commands.keys()).forEach(idName => {
-                if(util.permCheck(message,idName)){
-                    commands += `${idName}: ${client.commands.get(idName).desc}\n`
-                }               
-            })
-            message.author.send(commands, {code:"xl",split:true});
+        }catch(e){
+            util.log(client,`${e}\nSource: ${__filename.split('/root/bots/')[1]}`)
         }
     }
 }
