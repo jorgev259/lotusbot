7e57bcad8a131ec72fb3427ff3c499234969fef7
rikumax25 <jorgev259@outlook.es>
2018-01-12 14:58:09 -0600

    test commit

--- commands/quote.js
+++ commands/quote.js
@@ -1,26 +1,25 @@
 const Discord = require('discord.js');
+var { MessageEmbed } = require('discord.js');
 var quotes = require("../../data/quotes.json");
 
 module.exports = {
     desc:"This is a description",
     execute(client, message, param){
-try{
-        var quote = quotes[parseInt(param[1])];
-        if(quote != undefined && quote != null){
-            var embed = new Discord.MessageEmbed()
-            .setColor(quote.color)
-            .setDescription(quote.desc)
-            .setTitle(quote.title)
-            .setThumbnail(quote.thumb)
-            .setAuthor(quote.author, quote.avatar);
-            message.channel.send({embed});
-        }else{
-            message.reply("invalid Quote id");
+        try{
+            var quote = quotes[parseInt(param[1])];
+            if(quote != undefined && quote != null){
+                var embed = new MessageEmbed()
+                .setColor(quote.color)
+                .setDescription(quote.desc)
+                .setTitle(quote.title)
+                .setThumbnail(quote.thumb)
+                .setAuthor(quote.author, quote.avatar);
+                message.channel.send({embed});
+            }else{
+                message.reply("invalid Quote id");
+            }
+        }catch(e){
+            util.log(client,`${e}\nSource: ${__filename.split('/root/bots/')[1]}`)
         }
     }
-catch(e){
-util.log(client,`${e}
-Source: ${__filename.split('/root/bots/')[1]}`)
-}
-}
 }
