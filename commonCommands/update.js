ef4db74c780c77f8854f47d2978d066ecdb714f0
rikumax25 <jorgev259@outlook.es>
2018-01-16 13:57:12 -0600

    restart bot on succesfull pull

--- commonCommands/update.js
+++ commonCommands/update.js
@@ -17,34 +17,7 @@ module.exports = {
 					console.log(res);
 					if(res.files.length>0){
 						m.edit(`Git pull successful!\nModified files: ${res.files.join(" ,")}\nSummary: ${JSON.stringify(res.summary).split("{")[1].split("}")[0]}`);
-						res.files.forEach(file => {
-
-							var commandsPath = path.resolve("../", client.user.username.toLowerCase())
-							var modulePath;
-							var commandName;
-
-							if(file.startsWith("commands")){ 
-								commandName = file.split("commands/")[1];     
-								commandsPath = path.resolve(commandsPath, "commands");                       
-							}else if(file.startsWith("commonCommands")){ 
-								commandName = file.split("commonCommands/")[1];
-								commandsPath = path.resolve(commandsPath, "commonCommands");
-							}else{
-								return;
-							}
-
-							modulePath = `${commandsPath}/${commandName}`;
-							delete require.cache[require.resolve(modulePath)];
-
-							const command = require(modulePath);
-
-							client.commands.set(commandName.split(".js")[0], command);
-							if(command.alias){
-								command.alias.forEach(alias => client.commands.set(alias, command))
-							}
-							util.log(client, `Reloaded ${commandName}`)
-
-						})
+						process.exit()
 					}else{
 						m.edit("Already up to date!");
 					}
