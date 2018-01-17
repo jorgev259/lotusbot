32e1053e4be63880635ce4b6f68c64a598da34ae
rikumax25 <jorgev259@outlook.es>
2018-01-17 08:34:22 -0600

    use glob to check if badge/background exists

--- commands/background.js
+++ commands/background.js
@@ -6,31 +6,29 @@ module.exports = {
     desc:"This is a description",
     alias:["bg"],
     execute(client, message, param){
-try{
-        var inventory = json.readFileSync("../data/inventory.json");
-        var exp = json.readFileSync("../data/exp.json");
+        try{
+            var inventory = json.readFileSync("../data/inventory.json");
+            var exp = json.readFileSync("../data/exp.json");
 
-        if(param.length > 1){
-            var code = param[1].toUpperCase();
-
-            if(fs.existsSync(`../akira/images/backgrounds/${code}.png`) || code=="DEFAULT"){
-                if(inventory[message.author.id].bgs.includes(code) || code=="DEFAULT"){
-                    exp[message.author.id].bg = code;
-                    util.save(exp,"exp");
-                    message.channel.send("New background applied!")
+            if(param.length > 1){
+                var code = param[1].toUpperCase();
+                 
+                if(!glob.sync(`images/badges/**/${name}*`) || code=="DEFAULT"){
+                    if(inventory[message.author.id].bgs.includes(code) || code=="DEFAULT"){
+                        exp[message.author.id].bg = code;
+                        util.save(exp,"exp");
+                        message.channel.send("New background applied!")
+                    }else{
+                        message.channel.send("Sorry, you dont own this background ;-;");
+                    }
                 }else{
-                    message.channel.send("Sorry, you dont own this background ;-;");
+                    message.channel.send(`The background code ${code} doesnt exist. Check https://www.fandomcircle.com/shop-1#PROFILES for more info`)
                 }
             }else{
-                message.channel.send(`The background code ${code} doesnt exist. Check https://www.fandomcircle.com/shop-1#PROFILES for more info`)
+                message.channel.send("You forgot the background's code. Usage: >background <code>");
             }
-        }else{
-            message.channel.send("You forgot the background's code. Usage: >background <code>");
+        }catch(e){
+            util.log(client,`${e}\nSource: ${__filename.split('/root/bots/')[1]}`)
         }
     }
-catch(e){
-util.log(client,`${e}
-Source: ${__filename.split('/root/bots/')[1]}`)
-}
-}
 }
