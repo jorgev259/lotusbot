c0330a7bd1ca08c4c82031b5516aa5adff198bdb
rikumax25 <jorgev259@outlook.es>
2018-01-19 11:36:21 -0600

    ignore commonCommands

--- commonCommands/help.js
+++ commonCommands/help.js
@@ -1,4 +1,5 @@
-var util = require("../utilities.js")
+var util = require("../utilities.js");
+var ignore = ["help","reload","update"];
 
 module.exports = {
     desc:"This command displays information about a command. Usage: >help command",
@@ -9,13 +10,15 @@ module.exports = {
                     message.author.send(client.commands.get(param[1].toLowerCase()).desc);
                 }
             }else{
-                var commands = ""
+                var commands = "";
+                var send = false;
                 Array.from(client.commands.keys()).forEach(idName => {
-                    if(util.permCheck(message,idName)){
+                    if(util.permCheck(message,idName) && ignore.includes(idName)){
+                        send=true;
                         commands += `${idName}: ${client.commands.get(idName).desc}\n`
                     }               
                 })
-                message.author.send(commands, {code:"xl",split:true});
+                if(send) message.author.send(commands, {code:"xl",split:true});
             }
         }catch(e){
             util.log(client,`${e}\nSource: ${__filename.split('/root/bots/')[1]}`)
