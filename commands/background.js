9fcc58f58730265ca01da23b9c23d438c47cf9e5
rikumax25 <jorgev259@outlook.es>
2018-01-19 09:16:12 -0600

    oops

--- commands/background.js
+++ commands/background.js
@@ -13,8 +13,8 @@ module.exports = {
 
             if(param.length > 1){
                 var code = param[1].toUpperCase();
-                 
-                if(glob.sync(`images/badges/**/${code}*`).length || code=="DEFAULT"){
+                 //"images/backgrounds/*/FHF*"
+                if(glob.sync(`images/backgrounds/**/${code}*`).length || code=="DEFAULT"){
                     if(inventory[message.author.id].bgs.includes(code) || code=="DEFAULT"){
                         exp[message.author.id].bg = code;
                         util.save(exp,"exp");
