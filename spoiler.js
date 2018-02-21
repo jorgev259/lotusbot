b1f2fa98d3057f0bfab9b62fc54c874a744d6160
rikumax25 <jorgev259@outlook.es>
2018-02-21 13:45:22 -0600

    delete this

--- spoiler.js
+++
@@ -1,5 +0,0 @@
-const SpoilerBot = require('discord-spoiler-bot');
-module.exports = function(){  
-    let bot = new SpoilerBot({token: (require("../data/tokens.json").akira)});
-    bot.connect();
-}
