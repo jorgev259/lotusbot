ccdea4f4e39276a0fedd0e1d0c69237936f48f3a
PenguinArctic <36924789+PenguinArctic@users.noreply.github.com>
2018-02-28 22:53:28 +0800

    deleted try-catch

--- commands/simple.js
+++ commands/simple.js
@@ -2,11 +2,7 @@ var commands = require("../../data/commands.json");
 
 module.exports = {
     execute(client, message, param){
-        try{
             var command = commands[param[0].toLowerCase()];
             message.channel.send(eval("`" + command.content + "`"));
-        }catch(e){
-            util.log(client,`${e}\nSource: ${__filename.split('/root/bots/')[1]}`)
-        }
     }
 }
