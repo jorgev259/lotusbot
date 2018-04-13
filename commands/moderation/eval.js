dd7865b86e9de651141a251cd16cd8baa4f9a887
rikumax25 <jorgev259@outlook.es>
2018-04-13 14:43:18 -0600

    add db to eval parameters

--- commands/moderation/eval.js
+++ commands/moderation/eval.js
@@ -1,6 +1,6 @@
 module.exports = {
     desc:"Runs the written code (Use with precaution). >eval <code>",
-    async execute(client, message, param){
+    async execute(client, message, param, db){
             param = param.slice(1)
             const code = param.join(" ");
             let evaled = eval(code);
