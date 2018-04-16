288e9fe772c5a073721bbfe5c2332d894c7e4a3b
rikumax25 <jorgev259@outlook.es>
2018-04-15 19:58:29 -0600

    oops

--- commands/fun/background.js
+++ commands/fun/background.js
@@ -9,7 +9,7 @@ module.exports = {
         if(param.length > 1){
             var code = param[1].toUpperCase();
             if(glob.sync(`images/backgrounds/**/${code}*`).length || code=="DEFAULT"){
-                var bgs = (await db.all(`SELECT item from inventory WHERE id=${message.author,id} AND type="bgs"`)).map(e=>e.item);
+                var bgs = (await db.all(`SELECT item from inventory WHERE id=${message.author.id} AND type="bgs"`)).map(e=>e.item);
 
                 if(bgs.includes(code) || code=="DEFAULT"){
                     await db.run(`UPDATE exp SET bg = "${code}" WHERE id = ${message.author.id}`);
