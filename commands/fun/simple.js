8d4736822bcab60ebf625eebc31ccfc1e26e56eb
rikumax25 <jorgev259@outlook.es>
2018-04-17 13:37:20 -0600

    error handling

--- commands/fun/simple.js
+++ commands/fun/simple.js
@@ -1,18 +1,18 @@
 function params(param){
     return param.slice(1,param.length).join(" ");
 };
+let hook;
 
 module.exports = {
     async execute(client, message, param){
         var commands = client.data.commands;
         var command = commands[param[0].toLowerCase()];
-
-        let hook = await message.channel.createWebhook("simple", { avatar: message.author.displayAvatarURL()});        
-        await hook.sendSlackMessage({
-            'username': message.member.nickname,
+        if(!hook) hook = await message.channel.createWebhook("simple", { avatar: message.author.displayAvatarURL()});  
+        else await hook.edit({'avatar': message.author.displayAvatarURL()}) 
+        message.delete();     
+        hook.sendSlackMessage({
+            'username': message.author.username,
             'text': eval("`" + command.content + "`")
-        });
-        message.delete();
-        hook.delete();
+        }).catch(console.error);   
     }
 }
