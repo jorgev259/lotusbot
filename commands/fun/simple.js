05d216a7fe9ad89c5b036820ae93048416df286c
rikumax25 <jorgev259@outlook.es>
2018-04-22 09:21:47 -0600

    divide simple and webhook messages

--- commands/fun/simple.js
+++ commands/fun/simple.js
@@ -1,18 +1,11 @@
-function params(param){
-    return param.slice(1,param.length).join(" ");
-};
-let hook;
-
-module.exports = {
-    async execute(client, message, param){
-        var commands = client.data.commands;
-        var command = commands[param[0].toLowerCase()];
-        if(!hook) hook = await message.channel.createWebhook("simple", { avatar: message.author.displayAvatarURL()});  
-        else await hook.edit({'avatar': message.author.displayAvatarURL()}) 
-        message.delete();     
-        hook.sendSlackMessage({
-            'username': message.member.nickname,
-            'text': eval("`" + command.content + "`")
-        }).catch(console.error);   
+function params(param){	 
+    return param.slice(1,param.length).join(" ");	     
+};	
+     
+module.exports = {	 
+    execute(client, message, param){	
+        var commands = client.data.commands;	         
+        var command = commands[param[0].toLowerCase()];	        
+        message.channel.send(eval("`" + command.content + "`"));
     }
 }
