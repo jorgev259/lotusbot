13e79d34e5e6e833f1818678d04eda9d75b5a219
rikumax25 <jorgev259@outlook.es>
2018-04-23 14:28:48 -0600

    why tho

--- commands/fun/top.js
+++ commands/fun/top.js
@@ -2,24 +2,17 @@ const {MessageEmbed} = require("discord.js");
 
 module.exports = {
     async execute(client, message, param, db){
-        const list = await db.all("SELECT id,exp FROM exp  ORDER BY exp DESC LIMIT 10");
-        let content="";
-        let count = 0;
-        list.forEach(async row => {
-            let member;
-            try{
-                member = await message.guild.members.fetch(row.id);
-            }catch(e){
-                member = {nickname: "Unknown Member"}
-
-            }
-            count++;
-
-            content += `${count}. ${member.nickname}: ${row.exp}\n`
+        db.all("SELECT id,exp FROM exp  ORDER BY exp DESC LIMIT 10").then(list =>{
+            let content="";
+            let count = 0;
+            list.forEach(async row => {
+                count++;
+                content += `${count}. ${member.nickname || member.user.username}: ${row.exp}\n`
+            })
+            message.channel.send(content)
+            let embed = new MessageEmbed();
+            embed.addField("Fandom Circle Ranking", content);
+            message.channel.send(embed);
         })
-
-        let embed = new MessageEmbed();
-        embed.addField("Fandom Circle Ranking", content);
-        message.channel.send(embed);
     }
 }
