c0cb791ac26e2914a854428a223f08a17f499f58
rikumax25 <jorgev259@outlook.es>
2018-04-25 19:43:16 -0600

    double trouble

--- commands/fun/profile.js
+++ commands/fun/profile.js
@@ -57,7 +57,7 @@ module.exports = {
             var percent;
             if(exp.lvl > 1) {
                 percent = ((exp.exp - levels[exp.lvl -1].exp) / (levels[exp.lvl].exp - levels[exp.lvl -1].exp))
-                pfCtx.drawImage(img, 746 - (435*percent), 601, (435*percent), 26);
+                pfCtx.drawImage(img, 746 - (435*percent*2), 601, (435*percent), 26);
             }else{
                 percent = ((exp.exp) / (levels[0].exp))
                 pfCtx.drawImage(img, 312, 601, (435*percent), 26);
