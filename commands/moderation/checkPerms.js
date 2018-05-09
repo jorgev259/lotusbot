dd747ed407ae824b902875cd82b97f302a924f9f
rikumax25 <jorgev259@outlook.es>
2018-05-09 14:13:06 -0600

    maybe?

--- commands/moderation/checkPerms.js
+++ commands/moderation/checkPerms.js
@@ -3,8 +3,8 @@ module.exports = {
     async execute(client, message, param, db){
         let dbPerms = await db.all(`SELECT item,type FROM perms WHERE command='${param[0].toLowerCase()}'`)
 		let perms = {channel: [], role:[], user:[]};
-		dbPerms.forEach(item => {
-			perms[item.type].push(item.item);
+		dbPerms.forEach(element => {
+			perms[element.type].push(element.item);
         })
         message.channel.send(JSON.stringify(perms));
     }
