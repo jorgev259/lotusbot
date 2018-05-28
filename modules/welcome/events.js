0993792ec68908c896b60184db87b041f2668bcb
rikumax25 <jorgev259@outlook.es>
2018-05-28 12:56:29 -0600

    update channel name

--- modules/welcome/events.js
+++ modules/welcome/events.js
@@ -1,7 +1,7 @@
 module.exports = {
     events:{
         async guildMemberAdd(client,db,member){
-            member.guild.channels.find(channel => channel.name=="main-lounge").send(`Welcome to Fandom Circle, ${member}! Have Fun`);
+            member.guild.channels.find(channel => channel.name=="│main-lounge").send(`Welcome to Fandom Circle, ${member}! Have Fun`);
         }
     }    
 }
