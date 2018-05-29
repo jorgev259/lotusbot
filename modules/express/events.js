bc909fa9f1687a0a7f3d0980348a90126de3c929
rikumax25 <jorgev259@outlook.es>
2018-05-29 14:29:03 -0600

    change port

--- modules/express/events.js
+++ modules/express/events.js
@@ -1,6 +1,7 @@
 var util = require('../../utilities.js');
 var express = require('express');
 var app = express();
+let fs = require("fs");
 
 module.exports = {
     events: {
@@ -13,9 +14,9 @@ module.exports = {
                     res.sendStatus(404);
             });
 
-            app.listen(3000, function() {
+            app.listen(8080, function() {
                 util.log(client, 'Request API up and running, sir!');
-            });
+            });           
         }
     }
 }
